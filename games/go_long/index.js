1201a56ee929b60dbdfc9d6716fe37729db29070
Tim Harshbarger <tsharsh@gmail.com>
2022-10-04 16:38:43 -0500

    Commit for games.  On branch main  Changes to be committed: 	new file:   games/big_foot/data.module.js 	new file:   games/big_foot/index.css 	new file:   games/big_foot/index.html 	new file:   games/big_foot/index.js 	new file:   games/farkle/index.css 	new file:   games/farkle/index.html 	new file:   games/farkle/index.js 	new file:   games/go_long/helpers.module.js 	new file:   games/go_long/index.css 	new file:   games/go_long/index.html 	new file:   games/go_long/index.js 	new file:   games/horrified/data.js 	new file:   games/horrified/index.css 	new file:   games/horrified/index.html 	new file:   games/horrified/index.js 	new file:   games/index.css 	new file:   games/index.html 	new file:   games/index.js 	new file:   games/terraforming_mars/index.css 	new file:   games/terraforming_mars/index.html 	new file:   games/terraforming_mars/index.js 	new file:   games/terraforming_mars/scoresheet.html 	new file:   games/tiny_epic_galaxies/index.css 	new file:   games/tiny_epic_galaxies/index.html 	new file:   games/tiny_epic_galaxies/index.js

---
+++ games/go_long/index.js
@@ -0,0 +1,117 @@
+import {qb, play, turnover, penalty, sack, kickoff, punt, fieldGoal} from "./helpers.module.js";
+
+// User Interface variables.
+
+let playType = document.querySelector("#play-type");
+let result1 = document.querySelector("#result1");
+let chkResult1 = document.querySelector("#chkResult1");
+let result2 = document.querySelector("#result2");
+let chkResult2 = document.querySelector("#chkResult2");
+let result3 = document.querySelector("#result3");
+let chkResult3 = document.querySelector("#chkResult3");
+let bigPlay = document.querySelector("#bigPlay");
+let chkBigPlay = document.querySelector("#chkBigPlay");
+let messages = document.querySelector("#messages");
+let actionButtons = document.querySelector('[data-role="actions"]');
+
+// Helper functions.
+let hike = function () {
+  let type = qb();
+  let value = play();
+
+  playType.textContent = type;
+  if (!chkResult1.checked) {
+    result1.textContent = `${value.play[0]} yards`;
+  } // end if.
+  if (!chkResult2.checked) {
+    result2.textContent = `${value.play[1]} yards`;
+  } // end if.
+  if (!chkResult3.checked) {
+    result3.textContent = `${value.play[2]} yards`;
+  } // end if.
+  if (!chkBigPlay.checked) {
+    bigPlay.textContent = `${value.bigPlay} yards`;
+  } // end if.
+  return type;
+}; // end Hike.
+
+let resetDowns = function () {
+  document.querySelectorAll(`[type="checkbox"]`).forEach((el) => {
+    el.checked = false;
+  }); // end forEach.
+}; // end resetDowns.
+
+let playPenalty = function () {
+  let value = penalty();
+  messages.innerHTML = `${value.type} called on the ${value.team} for ${value.yardage} yards.`;
+}; // end playPenalty.
+
+let processHike = function (playValue) {
+  let value;
+
+  switch (playValue) {
+    case "penalty":
+      playPenalty();
+      break;
+    case "sacked":
+      value = sack();
+      messages.innerHTML = `The quarterback is sacked for a loss of ${value} yards!`;
+      break;
+    case "turnover":
+      value = turnover();
+      messages.innerHTML = `Turnover! The ball is recovered by the ${value}.`;
+      break;
+    default:
+      // Do nothing.
+  } // end switch playValue.
+}; // end processHike.
+
+// Main.
+
+actionButtons.addEventListener("click", function (event) {
+  let action = event.target.getAttribute("data-action");
+  // do nothing if a button is not clicked.
+  if (!action) {
+    return;
+  } // end if.
+
+  let value;
+  switch (action) {
+    case "hike":
+      value = hike();
+      processHike(value);
+      break;
+    case "fresh downs":
+      resetDowns();
+      value = hike();
+      processHike(value);
+      break;
+    case "kickoff":
+      value = kickoff();
+      messages.innerHTML = `The kick is returned to the ${value} yard line.`;
+      break;
+    case "punt":
+      value = prompt("Kicking from what absolute yardline?");
+      value = punt(value);
+      if (value.blocked) {
+        messages.innerHTML = `The defense blocks the punt!`;
+        break;
+      } // end if.
+      if (value.touchback) {
+        messages.innerHTML = `Touchback!`;
+        break;
+      } // end if.
+      messages.innerHTML = `The punt eends up at the ${value.total}`;
+      break;
+    case "field goal":
+      value = prompt("From where are you attempting the field goal?");
+      value = fieldGoal(value);
+      if (value) {
+        messages.innerHTML = `The kick is good!`;
+      } else {
+        messages.innerHTML = `The kick misses the uprights!`;
+      } // end if else.
+      break;
+    default:
+  } // end switch.
+}); // end click action buttons.
