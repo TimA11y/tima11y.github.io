dc2130ce65a7986c7f15f88b205ec9eb51e579d4
Tim Harshbarger <tsharsh@gmail.com>
2022-11-06 03:47:54 -0600

    Completed Week 3 Part 3 lesson on adding event listeners to web components.  On branch w3p3  Changes to be committed: 	modified:   solutions/roll-dice/index.js

--- solutions/roll-dice/index.js
+++ solutions/roll-dice/index.js
@@ -1,13 +1,45 @@
 class RollDice extends HTMLElement {
+
+
   constructor() {
     super();
-    let buttonLabel = (this.innerHTML) ? this.innerHTML : "Roll a dice!";
-    this.innerHTML = `
-    <p>
-      <button>${buttonLabel}</button>
-    </p>
-    <div class="message" aria-live="polite"></div>`;
+    let buttonLabel = (this.innerHTML) ? this.innerHTML.trim() : "Roll a dice!";
+    this.innerHTML = `<p><button>${buttonLabel}</button></p><div aria-live="polite" class="message"></div>`;
   } // end constructor method.
+
+  #roll() {
+    return Math.floor((Math.random() * 6) + 1);
+  } // end #roll method.
+
+  #clickButton(event) {
+    let parent = event.target.closest("roll-dice");
+    let msg = parent.querySelector(".message");
+    if (!msg) {
+      return;
+    } // end if.
+
+    msg.innerHTML = `You rolled a ${parent.#roll()}!`;
+  } // end #clickButton method.
+
+  connectedCallback() {
+    let btn = this.querySelector("button");
+    if (!btn) {
+      return;
+    } // end if.
+
+    btn.addEventListener("click", this.#clickButton);
+  } // end connectedCallback.
+
+  disconnectedCallback() {
+    let btn = this.querySelector("button");
+    if (!btn) {
+      return;
+    } // end if.
+
+    btn.removeEventListener("click", this.#clickButton);
+
+  } // end disconnectedCallback.
+
 } // end RollDice class.
 
 if ("customElements" in window) {
